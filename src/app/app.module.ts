5c81e44e8c94fdd22d59429fa1f5bbfcd126d0d4
Tomasz Wylężek <Tomasz.Wylezek@performgroup.com>
2018-05-19 10:38:43 +0200

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAPJaVR86XV0VC-wtvQqOGomvfIRrMW1XE',
+    authDomain: 'rx-dev-meeting.firebaseapp.com',
+    databaseURL: 'https://rx-dev-meeting.firebaseio.com',
+    projectId: 'rx-dev-meeting',
+    storageBucket: 'rx-dev-meeting.appspot.com',
+    messagingSenderId: '653633363816'
+  }
 };
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
