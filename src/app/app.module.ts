c54ddf087eec2eb51633da37fafab9c0071bdb76
Artur Omazda <artur@omazda.pl>
2018-05-19 12:47:09 +0200

    message view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {AppComponent} from './app.component';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {MsgformComponent} from './msgform/msgform.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule, MatInputModule, MatRippleModule, MatFormFieldModule} from '@angular/material';
+import {MatButtonModule, MatInputModule, MatRippleModule, MatFormFieldModule, MatCardModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -23,6 +23,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
     MatRippleModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
