27cbbcedd436ca7d5d95ec72ce16effd9ae8f555
Tomasz Wylężek <Tomasz.Wylezek@performgroup.com>
2018-05-19 12:05:21 +0200

    add modules for form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { environment } from '../environments/environment';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularFireModule} from 'angularfire2';
+import {environment} from '../environments/environment';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
-import { MsgformComponent } from './msgform/msgform.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MsgformComponent} from './msgform/msgform.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatInputModule, MatRippleModule, MatFormFieldModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -17,10 +18,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
