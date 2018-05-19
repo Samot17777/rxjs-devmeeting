6ebd6fb9831070ddf7a46180c2e48ec40ce6663f
Tomasz Wylężek <Tomasz.Wylezek@performgroup.com>
2018-05-19 10:56:51 +0200

    basic work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,6 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   items: Observable<any[]>;
   constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+    this.items = db.collection('msg').valueChanges().pipe(tap(console.log));
   }
 }
